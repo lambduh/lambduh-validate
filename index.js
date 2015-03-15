a7a3534129951b30948dd45f827541cb280d0504
Russell Matney <russell.matney@gmail.com>
2015-03-15 13:58:45 -0400

    tests for missing settings, missing validation keys

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 var Q = require('q');
 
-module.exports = function() {
-  return function() {
+module.exports = function(settings) {
+  return function(options) {
     var def = Q.defer();
+    if (!options) { options = {} }
+
+    if (!settings) {
+      def.resolve(options);
+    } else {
+      for (key in settings) {
+        if (!options[key]) {
+          def.reject(new Error('Validate expected options to include: ' + key));
+        }
+      }
+
+    }
+
     return def.promise;
   }
 }
--- test/validate-s3-key.spec.js
+++ test/validate-s3-key.spec.js
@@ -14,4 +14,34 @@ describe('validateS3Key', function() {
   it('should return a function that returns a promise', function() {
     expect(validate()().then).to.exist
   });
+
+  it('should resolve the options object if no settings are inputted', function(done) {
+    var options = {
+      key: 'val'
+    }
+    validate()(options).then(function(opts) {
+      if (opts) {
+        expect(opts).to.equal(options)
+        done()
+      } else {
+        done(new Error("Expected options object to be resolved."))
+      }
+    }, function() {
+      done(new Error("Expected options object to be resolved."))
+    })
+  });
+
+  it('should reject with an err if no options are inputted but requirements are set', function(done) {
+    var settings = {
+      srcKey: true
+    }
+    validate(settings)().then(function(opts) {
+      done(new Error("Expected scenario to fail."))
+    }, function(err) {
+      expect(err).to.exist
+      done()
+    })
+  });
+
+
 });
